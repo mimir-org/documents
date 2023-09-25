d916cdc1ae06148b8982c4a0af072715bf89ea4e
Mathias Bøe <mathias.boe@bouvet.no>
2023-09-25 14:09:14 +0200

    updating configs and github pages build workflow

--- docusaurus-docs/docusaurus.config.js
+++ docusaurus-docs/docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://www.github.com/',
+  url: 'https://www.mimir-org.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/mimir-org/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mimir-org', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'documents', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

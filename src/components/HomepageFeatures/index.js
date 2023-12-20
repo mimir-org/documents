c84dbe036330c02fe960bc7803507a321285ba43
Andreas Eikefet Kristensen <andreas.kristensen@bouvet.no>
2023-12-20 13:09:47 +0100

    Added url

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,6 +6,7 @@ const FeatureList = [
     {
         title: 'Mimir',
         img: require('@site/static/img/mimirDarkLogo.png').default,
+        url: '/documents/docs/Mimir/intro',
         description: (
             <>
                 Mimir can use the types from tyle to draw and explore IMF-models. This currently does not have a
@@ -16,29 +17,33 @@ const FeatureList = [
     {
         title: 'Tyle',
         img: "./img/tyleLogo.svg",
+        url: '/documents/docs/Tyle/contributing',
         description: (
             <>
-                Tyle is the type creating system of Mimir-org. Create types to use with Mimir
-                <a href={"www.imftools.com"}> here</a>.
+                Tyle is the type creating system of Mimir-org. Create types to use with Mimir.
             </>
         ),
     },
     {
         title: 'Component Library',
         img: require('@site/static/img/community.png').default,
+        url: 'https://mimir-org.github.io/component-library/?path=/docs/welcome--docs',
         description: (
             <>
-                This is the component library of both Mimir and Tyle, you can view the current components <a href={"https://mimir-org.github.io/component-library/"}> here</a>.
+                This is the component library of both Mimir and Tyle, you can view the current components <a
+                href={"https://mimir-org.github.io/component-library/"}> here</a>.
             </>
         ),
     },
 ];
 
-function Feature({img, title, description}) {
+function Feature({img, title, description, url}) {
     return (
         <div className={clsx('col col--4')}>
             <div className="text--center">
-                <img className={styles.featureImage} src={img} alt={"logo"}/>
+                <a href={url}>
+                    <img className={styles.featureImage} src={img} alt={"logo"}/>
+                </a>
             </div>
             <div className="text--center padding-horiz--md">
                 <h3>{title}</h3>
